bb62615df34730a24380c6a8379a3894fa85119d
Sanjipan <sanjipan2003@gmail.com>
2022-11-16 23:15:23 +0530

    update

--- NetworkPacketSniffer.py
+++ NetworkPacketSniffer.py
@@ -1,2 +1,9 @@
-import sys
 import socket
+import struct
+import textwrap
+
+# Unpack ethernet frame
+def ethernet_frame(data):
+    dest_mac,src_mac,proto =struct.unpack('! 6s 6s H', data[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
+
