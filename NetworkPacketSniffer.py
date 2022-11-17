d9b04354cc684e34ba1c49a53b72deac4f686504
Sanjipan_Deb <75313329+Sanjipan@users.noreply.github.com>
2022-11-17 13:28:47 +0530

    Complete

--- NetworkPacketSniffer.py
+++ NetworkPacketSniffer.py
@@ -21,20 +21,6 @@ def main():
         dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
         print("\nEthernet Frame: ")
         print("Destination: {}, Source: {}, Protocol: {}".format(dest_mac, src_mac, eth_proto))
-        
-        # 8 for IPv4
-        if eth_proto == 8:
-            (version, header_length, ttl, proto, src, target, data) = ipv4_packet(data)
-            print(TAB_1 + 'IPv4 Packet:')
-            print(TAB_2 + 'Version: {}, Header Length: {}, TTL: {}'.format(version, header_length, ttl))
-            print(TAB_2 + 'Protocol: {}, Source: {}, Target: {}'.format(proto, src, target))
-
-            if proto== 1:
-                icmp_type, code, checksum, data = icmp_packet(data)
-                print(TAB_1 + 'ICMP Packet:')
-                print(TAB_2 + 'Type: {}, code: {}, Checksum: {}'.format(icmp_type, code, checksum))
-                print(TAB_2 + 'Data: ')
-                print(format_multi_line(DATA_TAB_3, data))
 
         # 8 for IPv4
         if eth_proto == 8:
@@ -42,13 +28,42 @@ def main():
             print(TAB_1 + 'IPv4 Packet:')
             print(TAB_2 + 'Version: {}, Header Length: {}, TTL: {}'.format(version, header_length, ttl))
             print(TAB_2 + 'Protocol: {}, Source: {}, Target: {}'.format(proto, src, target))
-
-            if proto== 1:
+            # ICMP
+            if proto == 1:
                 icmp_type, code, checksum, data = icmp_packet(data)
                 print(TAB_1 + 'ICMP Packet:')
                 print(TAB_2 + 'Type: {}, code: {}, Checksum: {}'.format(icmp_type, code, checksum))
                 print(TAB_2 + 'Data: ')
                 print(format_multi_line(DATA_TAB_3, data))
+            # TCP
+            elif proto == 6:
+                (src_port, dest_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn,
+                 flag_fin, data) = tcp_segment(data)
+                print("TCP Segment:")
+                print(TAB_2 + "Source Port: {}, Destination port: {}".format(src_port, dest_port))
+                print(TAB_2 + "Sequence : {}, Acknowledgment: {},".format(sequence, acknowledgement))
+                print(TAB_2 + "Flags:")
+                print(
+                    TAB_3 + "URG : {}, ACK :{}, PSH :{}, RST :{}, SYN :{}, FIN :{}".format(flag_urg, flag_ack, flag_psh,
+                                                                                           flag_rst, flag_syn,
+                                                                                           flag_fin))
+                print(TAB_2 + "Data: ")
+                print(format_multi_line(DATA_TAB_3, data))
+            # UDP
+            elif proto == 17:
+                src_port, dest_port, size, data = udp_segment(data)
+                print(TAB_1 + "UDP Segment:")
+                print(TAB_2 + "Source Port: {}, Destination Port: {}, Length: {}".format(src_port, dest_port, size))
+                print(TAB_2 + "Data: ")
+                print(format_multi_line(DATA_TAB_3, data))
+            # Other
+            else:
+                print(TAB_1 + "Data :")
+                print(format_multi_line(DATA_TAB_2, data))
+        else:
+            print('Data :')
+            print(format_multi_line(DATA_TAB_1, data))
+
 
 # Unpack ethernet frame
 def ethernet_frame(data):
@@ -82,15 +97,35 @@ def icmp_packet(data):
     icmp_type, code, checksum = struct.unpack("! B B H", data[:4])
     return icmp_type, code, checksum, data[4:]
 
+
 # unpacks TCP segment
 def tcp_segment(data):
     (src_port, dest_port, sequence, acknowledgement, offset_reserved_flags) = struct.unpack("! H H L L H", data[:14])
-    offset = (offset_reserved_flags >> 12)*4
-    flag_urg = (offset_reserved_flags & 32) >>5
-    flag_ack = (offset_reserved_flags & 16) >>4
-    flag_psh = (offset_reserved_flags & 8) >>3
-    flag_rst = (offset_reserved_flags & 4) >>2
-    flag_syn = (offset_reserved_flags & 2) >>1
+    offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    flag_ack = (offset_reserved_flags & 16) >> 4
+    flag_psh = (offset_reserved_flags & 8) >> 3
+    flag_rst = (offset_reserved_flags & 4) >> 2
+    flag_syn = (offset_reserved_flags & 2) >> 1
     flag_fin = offset_reserved_flags & 1
-    return src_port, dest_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
+    return src_port, dest_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[
+                                                                                                                       offset:]
 
+
+# Unpack UDP segment
+def udp_segment(data):
+    src_port, dest_port, size = struct.unpack('! H H 2x H', data[:8])
+    return src_port, dest_port, size, data[8:]
+
+
+# Formating Multi line data
+def format_multi_line(prefix, string, size=80):
+    size -= len(prefix)
+    if isinstance(string, bytes):
+        string = ''.join(r'\x{:02x}'.format(byte) for byte in string)
+        if size % 2:
+            size -= 1
+    return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
+
+
+main()
