967a75281d961312209600f8c9eafd79bc87c455
Abanteeka <acharyaabanteeka196@gmail.com>
2022-11-17 10:18:53 +0530

    Initial Commit

--- NetworkPacketSniffer.py
+++ NetworkPacketSniffer.py
@@ -1,3 +1,20 @@
-import sys
 import socket
-import time
+import struct
+import textwrap
+
+
+def main():
+    conn=socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+# Unpack ethernet frame
+def ethernet_frame(data):
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
+
+
+# Return Properly The Mac Address(ie: AA;BB;CC;DD;EE;FF)
+def get_mac_addr(byte_addr):
+    byte_str = map('{:02x}'.format, byte_addr)
+    mac_addr = ':'.join(byte_str).upper()
+    return mac_addr
+
