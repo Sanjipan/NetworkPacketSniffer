99342da1279b90f4039de9d41cae7acff2ceb143
Abanteeka <acharyaabanteeka196@gmail.com>
2022-11-17 11:26:11 +0530

    def tcp_segment(data): updated

--- NetworkPacketSniffer.py
+++ NetworkPacketSniffer.py
@@ -37,3 +37,20 @@ def ipv4_packet(data):
 # Return properly formatted IPv4 address
 def ipv4(addr):
     return '.'.join(map(str, addr))
+
+# unpacks ICMP packet
+def icmp_packet(data):
+    icmp_type, code, checksum = struct.unpack("! B B H", data[:4])
+    return icmp_type, code, checksum, data[4:]
+
+# unpacks TCP segment
+def tcp_segment(data):
+    (src_port, dest_port, sequence, acknowledgement, offset_reserved_flags) = struct.unpack("! H H L L H", data[:14])
+    offset = (offset_reserved_flags >> 12)*4
+    flag_urg = (offset_reserved_flags & 32) >>5
+    flag_ack = (offset_reserved_flags & 16) >>4
+    flag_psh = (offset_reserved_flags & 8) >>3
+    flag_rst = (offset_reserved_flags & 4) >>2
+    flag_syn = (offset_reserved_flags & 2) >>1
+    flag_fin = offset_reserved_flags & 1
+    return src_port, dest_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
